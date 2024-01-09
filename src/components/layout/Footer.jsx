6fba6d38cc9a6da6856a86954eb85056de184edc
Erickmarlon006 <erickmarlonborbadesenna@gmail.com>
2024-01-09 19:30:05 -0300

    Footer utilizado nas páginas

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css"
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h4>Todos os direitos reservados Copyright &copy; 2024 Erick Marlon Borba de Senna</h4>
+        </footer>
+    )
+}
+export default Footer
