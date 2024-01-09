23dc3c03998d0239fdbc3490a1ca367ab7375edf
Erickmarlon006 <erickmarlonborbadesenna@gmail.com>
2024-01-09 19:33:37 -0300

    Página inicial da aplicação

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../layout/Header"
+import Content from "../Content"
+import Footer from "../layout/Footer"
+function Home() {
+    return (
+        <>
+            <Header />
+            <Content />
+            <Footer />
+        </>
+    )
+}
+export default Home
